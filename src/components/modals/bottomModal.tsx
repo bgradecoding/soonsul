d56fb0e3cbfeaade59921dbda695433852f06418
bgradecoding <iinee12@kakao.com>
2023-08-08 18:52:07 +0900

    :bug: (modal) 모달 외부 클릭 없어지는 현상 픽스

--- src/components/modals/bottomModal.tsx
+++ src/components/modals/bottomModal.tsx
@@ -24,13 +24,7 @@ const BottomModal: React.FC<SignupModalProps> = ({
     <>
       {isOpen ? (
         <>
-          <div
-            onClick={(event: any) => {
-              event.preventDefault();
-              closeModal(false);
-            }}
-            className="fixed inset-0 z-10 flex items-end overflow-x-hidden overflow-y-auto outline-none focus:outline-none bg-[#000000] bg-opacity-60"
-          >
+          <div className="fixed inset-0 z-10 flex items-end overflow-x-hidden overflow-y-auto outline-none focus:outline-none bg-[#000000] bg-opacity-60">
             <div className="relative w-full max-w-2xl mx-auto">
               <div
                 className="relative flex flex-col w-full bg-white border-0 shadow-lg outline-none focus:outline-none"
